62b49b792b0ca53f8a191b7646f7330eeb736210
Fred-Bred <frbredgaard@gmail.com>
2024-02-21 15:10:46 +0100

    update train/test script to copy rather than move

--- src/train_test_split.py
+++ src/train_test_split.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from sklearn.model_selection import train_test_split
 
 # Paths
@@ -8,10 +9,10 @@ test_dir = "/home/unicph.domain/wqs493/ucph/securegroupdir/SAMF-SODAS-PACS/PACS_
 
 # Create split
 file_list = os.listdir(data_dir)
-train_files, test_files = train_test_split(file_list, test_size=0.1, random_state=42)
+train_files, test_files = train_test_split(file_list, test_size=0.15, random_state=42)
 
-# Move files to respective directories
+# Copy files to respective directories
 for file in train_files:
-    os.rename(os.path.join(data_dir, file), os.path.join(train_dir, file))
+    shutil.copy2(os.path.join(data_dir, file), os.path.join(train_dir, file))
 for file in test_files:
-    os.rename(os.path.join(data_dir, file), os.path.join(test_dir, file))
+    shutil.copy2(os.path.join(data_dir, file), os.path.join(test_dir, file))
