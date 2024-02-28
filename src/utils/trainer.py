b8ad5edbb8f8bd35b7fffc0aa28785a99770e05d
Fred-Bred <frbredgaard@gmail.com>
2024-02-28 12:45:58 +0100

    Add metrics to Trainer

--- src/utils/trainer.py
+++ src/utils/trainer.py
@@ -8,10 +8,12 @@ import torch
 import torch.nn as nn
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+from torchmetrics import Accuracy, Precision, Recall, F1
 
 if __name__ == "__main__":
     # Set device
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    num_labels = 3
 
 # class Trainer:
 #     def __init__(self):
@@ -31,8 +33,24 @@ if __name__ == "__main__":
 #             self.model_name = "unspecified"
 #         else:
 #             self.model_name = model_name
- 
-#     def fit(self, num_epochs, train_loader, val_loader=None, patience=5, min_delta=0.0001):
+    
+#     def calculate_accuracy(self, logits, labels):
+#         preds = torch.argmax(logits, dim=1)
+#         return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+    
+#     def calculate_f1(self, logits, labels):
+#         preds = torch.argmax(logits, dim=1)
+#         return f1_score(labels, preds, average='weighted')
+    
+#     def calculate_precision(self, logits, labels):
+#         preds = torch.argmax(logits, dim=1)
+#         return precision_score(labels, preds, average='weighted')
+    
+#     def calculate_recall(self, logits, labels):
+#         preds = torch.argmax(logits, dim=1)
+#         return recall_score(labels, preds, average='weighted')
+
+#     def fit(self, num_epochs, train_loader, device, val_loader=None, patience=5, min_delta=0.0001):
 #         self.train_loader = train_loader
 #         self.val_loader = val_loader
 #         self.patience = patience
@@ -46,12 +64,14 @@ if __name__ == "__main__":
 #             self.model.train()
 #             total_loss = 0.0
 #             with tqdm(total=len(self.train_loader), desc=f"Epoch {epoch + 1}/{num_epochs}") as pbar:
-#                 for texts, labels in self.train_loader:
-#                     texts = texts.to(device)
-#                     labels = labels.to(device)
+#                 for batch in self.train_loader:
+#                     texts = batch['input_ids'].to(device)
+#                     attention_mask = batch['attention_mask'].to(device)
+#                     labels = batch['targets'].to(device)
 #                     self.optimizer.zero_grad()
-#                     outputs = self.model(texts)
-#                     loss = self.loss_fn(outputs, labels)
+#                     outputs = self.model(texts, attention_mask=attention_mask)
+#                     logits = outputs.logits
+#                     loss = self.loss_fn(logits, labels)
 #                     loss.backward()
 #                     self.optimizer.step()
 #                     total_loss += loss.item()
@@ -63,7 +83,7 @@ if __name__ == "__main__":
 #             print(f"Training Loss: {avg_loss}")
 
 #             if self.val_loader is not None:
-#                 val_loss = self.evaluate(self.val_loader, "Validation")
+#                 val_loss = self.evaluate(self.val_loader, device, "Validation")
 #                 self.history['val_loss'].append(val_loss)
 
 #                 # Plot the loss
@@ -73,7 +93,8 @@ if __name__ == "__main__":
 #                 plt.ylabel('Loss')
 #                 plt.title('Loss over epochs')
 #                 plt.legend()
-#                 plt.savefig(f'plots/loss_plot{str(figure_num)}.png')
+#                 os.makedirs('Outputs/Figures', exist_ok=True)
+#                 plt.savefig(f'Outputs/Figures/loss_plot{str(figure_num)}.png')
 #                 plt.clf()
 
 #                 # Check for early stopping
@@ -89,19 +110,18 @@ if __name__ == "__main__":
 #             ### save checkpoint of model and plot
 #             ### MODEL
 #             try:
-#                 os.makedirs('../trained_models', exist_ok=True)
-#                 model_name = f"{self.model_name}_checkpoint_EPOCH_{epoch}_SAMPLES_{len(self.data_loader.dataset)}_BATCHSIZE_{self.data_loader.batch_size}.pt"
-#                 torch.save(self.model.state_dict(), '../trained_models/' + model_name)
+#                 os.makedirs('Outputs/trained_models', exist_ok=True)
+#                 model_name = f"{self.model_name}_checkpoint_EPOCH_{epoch}_SAMPLES_{len(self.train_loader.dataset)}_BATCHSIZE_{self.train_loader.batch_size}.pt"
+#                 torch.save(self.model.state_dict(), 'Outputs/trained_models/' + model_name)
 #                 print(f'Checkpoint after epoch {epoch+1} saved successfully')
 #             except Exception as e:
 #                 print(f"Error saving checkpoint with name: {e}")
 #                 model_name = f"checkpoint_TIME_{datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}.pt"
-#                 torch.save(self.model.state_dict(), '../trained_models/' + model_name)
+#                 torch.save(self.model.state_dict(), 'Outputs/trained_models/' + model_name)
 #                 print('Unnamed checkpoint saved successfully')
 
-            
+#             ### PLOT
 #             try:
-#                 ### PLOT
 #                 history = self.history
 #                 # Plot the loss history
 #                 plt.plot(history['train_loss'], label='Train Loss')
@@ -110,21 +130,22 @@ if __name__ == "__main__":
 #                 plt.ylabel('Loss')
 #                 plt.title('Training History')
 #                 plt.legend()
-#                 plt.savefig(f"../trained_models/{self.model_name}_checkpoint_EPOCH_{epoch}_SAMPLES_{len(self.data_loader.dataset)}_BATCHSIZE_{self.data_loader.batch_size}.png")
+#                 plt.savefig(f"Outputs/trained_models/{self.model_name}_checkpoint_EPOCH_{epoch}_SAMPLES_{len(self.train_loader.dataset)}_BATCHSIZE_{self.train_loader.batch_size}.png")
 #             except:
 #                 print('Error generating plot')
 
-#     def evaluate(self, data_loader, mode="Test"):
+#     def evaluate(self, data_loader, device, mode="Test"):
 #         self.model.eval()
 #         total_loss = 0.0
 #         with torch.no_grad():
-#             for texts, labels in data_loader:
+#             for batch in data_loader:
 #                 # Move the data to the GPU
-#                 texts = texts.to(device)
-#                 labels = labels.to(device) 
-
-#                 outputs = self.model(texts)
-#                 loss = self.loss_fn(outputs, labels)
+#                 texts = batch['input_ids'].to(device)
+#                 attention_mask = batch['attention_mask'].to(device)
+#                 labels = batch['targets'].to(device)
+#                 outputs = self.model(texts, attention_mask=attention_mask)
+#                 logits = outputs.logits
+#                 loss = self.loss_fn(logits, labels)
 #                 total_loss += loss.item()
 
 #         avg_loss = total_loss / len(data_loader)
@@ -157,7 +178,21 @@ class Trainer:
         self.train_loader = None
         self.val_loader = None
         self.source = None
-        self.history = {'train_loss': [], 'val_loss': [], 'train_loss_batch': [], 'val_loss_batch': []}
+        self.accuracy = Accuracy(average='weighted', num_classes=num_labels)
+        self.precision = Precision(average='weighted', num_classes=num_labels)
+        self.recall = Recall(average='weighted', num_classes=num_labels)
+        self.history = {
+            'train_loss': [],
+            'val_loss': [],
+            'train_loss_batch': [],
+            'val_loss_batch': [],
+            'train_acc': [],
+            'val_acc': [],
+            'train_precision': [],
+            'val_precision': [],
+            'train_recall': [],
+            'val_recall': []
+        }
 
     def compile(self, model, optimizer, learning_rate, loss_fn, weight_decay=0.01, model_name=None):
         self.model = model
@@ -167,7 +202,7 @@ class Trainer:
             self.model_name = "unspecified"
         else:
             self.model_name = model_name
- 
+
     def fit(self, num_epochs, train_loader, device, val_loader=None, patience=5, min_delta=0.0001):
         self.train_loader = train_loader
         self.val_loader = val_loader
@@ -183,25 +218,56 @@ class Trainer:
             total_loss = 0.0
             with tqdm(total=len(self.train_loader), desc=f"Epoch {epoch + 1}/{num_epochs}") as pbar:
                 for batch in self.train_loader:
+                    # Make predictions
                     texts = batch['input_ids'].to(device)
                     attention_mask = batch['attention_mask'].to(device)
                     labels = batch['targets'].to(device)
                     self.optimizer.zero_grad()
                     outputs = self.model(texts, attention_mask=attention_mask)
                     logits = outputs.logits
+
+                    # Calculate train metrics
                     loss = self.loss_fn(logits, labels)
+                    train_acc = self.accuracy(logits.softmax(dim=1), labels)
+                    train_precision = self.precision(logits.softmax(dim=1), labels)
+                    train_recall = self.recall(logits.softmax(dim=1), labels)
+
+                    # Backpropagation
                     loss.backward()
                     self.optimizer.step()
                     total_loss += loss.item()
 
                     # Update the progress bar
                     pbar.update(1)
+            
+            # Compute metrics
             avg_loss = total_loss / len(self.train_loader)
-            self.history['train_loss'].append(avg_loss)
-            print(f"Training Loss: {avg_loss}")
+            train_acc = self.accuracy.compute()
+            train_precision = self.precision.compute()
+            train_recall = self.recall.compute()
 
+            # Add metrics to history
+            self.history['train_loss'].append(avg_loss)
+            self.history['train_acc'].append(train_acc)
+            self.history['train_precision'].append(train_precision)
+            self.history['train_recall'].append(train_recall)
+
+            # Print metrics
+            print(f"Training Loss: {avg_loss}")
+            print(f"Training Accuracy: {train_acc}")
+            print(f"Training Precision: {train_precision}")
+            print(f"Training Recall: {train_recall}")
+
+            # Reset metrics
+            self.accuracy.reset()
+            self.precision.reset()
+            self.recall.reset()
+            
             if self.val_loader is not None:
+                # Calculate validation loss
                 val_loss = self.evaluate(self.val_loader, device, "Validation")
+
+                # Add val loss to history
                 self.history['val_loss'].append(val_loss)
 
                 # Plot the loss
@@ -261,13 +327,41 @@ class Trainer:
                 texts = batch['input_ids'].to(device)
                 attention_mask = batch['attention_mask'].to(device)
                 labels = batch['targets'].to(device)
+
+                # Make predictions
                 outputs = self.model(texts, attention_mask=attention_mask)
                 logits = outputs.logits
+
+                # Calculate metrics
                 loss = self.loss_fn(logits, labels)
                 total_loss += loss.item()
+                val_acc = self.accuracy(logits.softmax(dim=1), labels)
+                val_precision = self.precision(logits.softmax(dim=1), labels)
+                val_recall = self.recall(logits.softmax(dim=1), labels)
 
+        # Compute metrics
         avg_loss = total_loss / len(data_loader)
+        val_acc = self.accuracy.compute()
+        val_precision = self.precision.compute()
+        val_recall = self.recall.compute()
+
+        if mode == "Validation":
+            # Add metrics to history
+            self.history['val_acc'].append(val_acc)
+            self.history['val_precision'].append(val_precision)
+            self.history['val_recall'].append(val_recall)
+
+        # Print metrics
         print(f"{mode} Loss: {avg_loss}")
+        print(f"{mode} Accuracy: {val_acc}")
+        print(f"{mode} Precision: {val_precision}")
+        print(f"{mode} Recall: {val_recall}")
+
+        # Reset metrics
+        self.accuracy.reset()
+        self.precision.reset()
+        self.recall.reset()
+
         return avg_loss
     
     def save(self, filepath):
