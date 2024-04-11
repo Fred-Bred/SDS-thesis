218b7b246b6c96c9f9d44eeb638c973e28abe1a4
Fred-Bred <frbredgaard@gmail.com>
2024-04-11 10:48:21 +0200

    Add script to run training on multiple dataset configurations

---
+++ src/run.sh
@@ -0,0 +1,10 @@
+
+python3 train.py --dataset_configs configs/split1/split1_full.json
+
+python3 train.py --dataset_configs configs/split2/split2_full.json
+
+python3 train.py --dataset_configs configs/split3/split3_full.json
+
+python3 train.py --dataset_configs configs/split4/split4_full.json
+
+python3 train.py --dataset_configs configs/split5/split5_full.json
