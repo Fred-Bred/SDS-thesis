8eeb5af8225e20b4c63a3eb5aed4ea25049abc7b
Fred-Bred <frbredgaard@gmail.com>
2024-04-12 21:48:08 +0200

    Add new bash scripts for CV predictions on different min_lens

--- src/bash scripts/run.sh
+++ src/bash scripts/run.sh
@@ -1,9 +1,19 @@
 ./cv_250.sh
 
+./cv_predict_250.sh
+
 ./cv_150.sh
+
+./cv_predict_150.sh
 
 ./cv_100.sh
 
+./cv_predict_100.sh
+
 ./cv_50.sh
 
+./cv_predict_50.sh
+
 ./cv_full.sh
+
+./cv_predict_full.sh
