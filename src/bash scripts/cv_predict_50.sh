337893f6538e024c233bf33a3879352e6d572598
Fred-Bred <frbredgaard@gmail.com>
2024-04-18 09:32:12 +0200

    Refactor cv_predict scripts to use MentalRoBERTa instead of roberta-large in output directory names

--- src/bash scripts/cv_predict_50.sh
+++ src/bash scripts/cv_predict_50.sh
@@ -18,7 +18,7 @@ for parent_dir in "${parent_dirs[@]}"; do
             # Check if the file name contains the search string
             if [[ $file == *"$search_string"* ]]; then
                 # Define the output directory
-                output_dir="../Outputs/trained_models/k-folds/roberta-large_50"
+                output_dir="../Outputs/trained_models/k-folds/MentalRoBERTa_50"
                 
                 # Create the output directory if it doesn't exist
                 mkdir -p "$output_dir"
