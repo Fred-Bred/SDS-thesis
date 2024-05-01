e61ebbe3de1ae397d3b0c99ec8f87aa3793b279a
Fred-Bred <frbredgaard@gmail.com>
2024-05-01 08:51:02 +0200

    Fix recreate4_50.sh typo

--- src/bash scripts/recreate4_50.sh
+++ src/bash scripts/recreate4_50.sh
@@ -55,32 +55,32 @@ search_string="model_"
     #     done
     # fi
 
-# Clean roberta
-python3 train.py --dataset_configs configs/split4/split4_50.json --parameters_config configs/params_roberta.json
+# # Clean roberta
+# python3 train.py --dataset_configs configs/split4/split4_50.json --parameters_config configs/params_roberta.json
 
-# Get the latest subdirectory of the parent directory
-    dir=$(ls -td "$parent_dir"/* | head -n1)
+# # Get the latest subdirectory of the parent directory
+#     dir=$(ls -td "$parent_dir"/* | head -n1)
     
-    # Check if it's a directory
-    if [ -d "$dir" ]; then
-        # Loop over all files in the subdirectory
-        for file in "$dir"/*; do
-            # Check if the file name contains the search string
-            if [[ $file == *"$search_string"* ]]; then
-                # Define the output directory
-                output_dir="../Outputs/trained_models/k-folds/roberta-base_50"
+#     # Check if it's a directory
+#     if [ -d "$dir" ]; then
+#         # Loop over all files in the subdirectory
+#         for file in "$dir"/*; do
+#             # Check if the file name contains the search string
+#             if [[ $file == *"$search_string"* ]]; then
+#                 # Define the output directory
+#                 output_dir="../Outputs/trained_models/k-folds/roberta-base_50"
                 
-                # Create the output directory if it doesn't exist
-                mkdir -p "$output_dir"
+#                 # Create the output directory if it doesn't exist
+#                 mkdir -p "$output_dir"
                 
-                python3 predict.py "$file" "../Data/k-folds/split4/val_50.csv" "$output_dir/split4_val_preds.csv"
-                break
-            fi
-        done
-    fi
+#                 python3 predict.py "$file" "../Data/k-folds/split4/val_50.csv" "$output_dir/split4_val_preds.csv"
+#                 break
+#             fi
+#         done
+#     fi
 
 # roberta-large
-python3 train.py --dataset_configs configs/split4/split4_50.json --parameters_config configs/params_roberta_large.json
+python3 train.py --dataset_configs configs/split4/split4_50.json --parameters_config configs/params_roberta-large.json
 
 # Get the latest subdirectory of the parent directory
     dir=$(ls -td "$parent_dir"/* | head -n1)
