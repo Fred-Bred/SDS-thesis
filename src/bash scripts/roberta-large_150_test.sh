b50dcca6985940e148c1ba57ef4b68da510da96f
Fred-Bred <frbredgaard@gmail.com>
2024-05-06 10:12:36 +0200

    roberta-large_150 test preds script

---
+++ src/bash scripts/roberta-large_150_test.sh
@@ -0,0 +1,9 @@
+python3 predict.py "logs/split1_150/2024.04.13_17.22.35/model_2.pt" "../Data/PACS_varying_lengths/test_combined_150.csv" "../Outputs/trained_models/k-folds/roberta-large_150/split1_original_test_preds.csv"
+
+python3 predict.py "logs/split2_150/2024.04.13_20.39.14/model_18.pt" "../Data/PACS_varying_lengths/test_combined_150.csv" "../Outputs/trained_models/k-folds/roberta-large_150/split2_original_test_preds.csv"
+
+python3 predict.py "logs/split3_150/2024.04.14_00.59.29/model_7.pt" "../Data/PACS_varying_lengths/test_combined_150.csv" "../Outputs/trained_models/k-folds/roberta-large_150/split3_original_test_preds.csv"
+
+python3 predict.py "logs/split4_150/2024.04.14_04.06.59/model_2.pt" "../Data/PACS_varying_lengths/test_combined_150.csv" "../Outputs/trained_models/k-folds/roberta-large_150/split4_original_test_preds.csv"
+
+python3 predict.py "logs/split5_150/2024.04.14_07.11.25/model_4.pt" "../Data/PACS_varying_lengths/test_combined_150.csv" "../Outputs/trained_models/k-folds/roberta-large_150/split5_original_test_preds.csv"
