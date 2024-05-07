4a0f59dc710be36de84ec119919f2377629e0495
Fred-Bred <frbredgaard@gmail.com>
2024-05-07 16:02:23 +0200

    Add test prediction scripts for roberta-large_0 model

---
+++ src/bash scripts/roberta-large_0_test.sh
@@ -0,0 +1,5 @@
+python3 predict.py "logs/split1_full/2024.04.16_10.42.43/model_1.pt" "../Data/test_PACS.csv" "../Outputs/trained_models/k-folds/roberta-large_full/split1_original_test_preds.csv"
+python3 predict.py "logs/split2_full/2024.04.17_03.46.23/model_5.pt" "../Data/test_PACS.csv" "../Outputs/trained_models/k-folds/roberta-large_full/split2_original_test_preds.csv"
+python3 predict.py "logs/split3_full/2024.04.17_09.55.42/model_16.pt" "../Data/test_PACS.csv" "../Outputs/trained_models/k-folds/roberta-large_full/split3_original_test_preds.csv"
+python3 predict.py "logs/split4_full/2024.04.17_17.24.50/model_4.pt" "../Data/test_PACS.csv" "../Outputs/trained_models/k-folds/roberta-large_full/split4_original_test_preds.csv"
+python3 predict.py "logs/split5_full/2024.04.18_00.40.14/model_9.pt" "../Data/test_PACS.csv" "../Outputs/trained_models/k-folds/roberta-large_full/split5_original_test_preds.csv"
